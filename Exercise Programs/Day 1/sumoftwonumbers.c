204474d5ac59eb8097aff8157f050b4b4c65977e
Roseline J <roseline.j@henkel.com>
2025-09-03 22:39:34 +0530

    Day 1 Exercise & Homework Programs

---
+++ Exercise Programs/Day 1/sumoftwonumbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    // Expression that uses values
+    int sum1 = 20 + 40;
+
+    // Defining variables
+    int a = 20, b = 40;
+
+    // Expression that uses variables
+    int sum2 = a + b;
+
+    // Output the results
+    printf("Sum of 2 numbers is: %d", sum1);
+    printf("\n\nSum of another 2 numbers is: %d", sum2);
+
+    return 0;
+}
+
+
+// -------------------------------------------------------------------------------------
+
+// Output :
+
+Sum of 2 numbers is : 60
+
+Sum of another 2 numbers is : 60
