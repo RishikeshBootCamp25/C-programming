5b8f5f1a1ed00355ba86aa03795aaded035a9930
Roseline-J <roseline.j@henkel.com>
2025-09-16 23:44:08 +0530

    Update create_personal_details.c

---
+++ Homework Assignment/Day 1/create_personal_details.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Declaring variables
+    char name[] = "Anu";
+    int age = 25;
+    char gender[] = "Female";
+    char address[] = "Thrissur, Kerala";
+
+    // Printing personal details
+    printf("---- Personal Details ----\n");
+    printf("Name     : %s\n", name);
+    printf("Age      : %d\n", age);
+    printf("Gender   : %s\n", gender);
+    printf("Address  : %s\n", address);
+
+    return 0;
+}
